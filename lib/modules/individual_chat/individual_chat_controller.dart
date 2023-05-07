f12c55c8b9c1db0b9e4a20fbfc98c1645bd3cce1
OscarCubeles <oscar.cubele@students.salle.url.edu>
2023-05-07 18:06:59 +0200

    feat(chat): screen done, need some bug fixes and functionalities

--- lib/modules/individual_chat/individual_chat_controller.dart
+++ lib/modules/individual_chat/individual_chat_controller.dart
@@ -1,5 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class IndividualChatController extends GetxController{
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void onReady() {
+    scrollToBottom();
+  }
+
+  void scrollToBottom() {
+    scrollController.animateTo(
+      scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 1),
+      curve: Curves.easeOut,
+    );
+  }
 
 }
--- lib/modules/individual_chat/individual_chat_screen.dart
+++ lib/modules/individual_chat/individual_chat_screen.dart
@@ -1,16 +1,206 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:hundopt/modules/individual_chat/individual_chat_controller.dart';
 
-class IndividualChatScreen extends GetView<IndividualChatController>{
+import '../../shared/constants/colors.dart';
+
+class IndividualChatScreen extends GetView<IndividualChatController> {
   const IndividualChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Single chat"),
-    );
+        body: Container(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey)),
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () => Get.back(),
+                    color: ColorConstants.appColor,
+                  ),
+                  SizedBox(width: 8),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      'https://picsum.photos/id/237/200/200',
+                      width: 48,
+                      height: 48,
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'John Doe',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        'United States',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://www.countryflags.io/us/flat/64.png'),
+                      radius: 20,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+                  controller: controller.scrollController,
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                if (index % 2 == 0) {
+                  // received message
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              color: Colors.lightGreen,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(16),
+                              ),
+                              border: Border.all(color: Colors.red),
+                            ),
+                            child: Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel velit convallis, viverra arcu ac, ultricies odio.',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '11:30 AM',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                } else {
+                  // sent message
+                  return Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            padding: EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                              color: Colors.lightGreen,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(16),
+                              ),
+                              border: Border.all(color: Colors.red),
+                            ),
+                            child: Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel velit convallis, viverra arcu ac, ultricies odio.',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white),
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            '11:30 AM',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
+            )),
+            Container(
+              height: 50,
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+              padding: EdgeInsets.fromLTRB(16, 0, 8, 0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(32),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      cursorColor: ColorConstants.appColor,
+                      decoration: InputDecoration(
+                        hintText: 'Write a message...',
+                        border: InputBorder.none,
+                        isCollapsed: true,
+                      ),
+                      style: Theme.of(context).textTheme.labelMedium,
+                      minLines: 1,
+                      maxLines: 5,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // handle button press
+                    },
+                    child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: ColorConstants.appColor,
+                        ),
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 2, 2, 0),
+                              child: Icon(
+                                CupertinoIcons.paperplane,
+                                color: Colors.white,
+                              ),
+                            ))),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
-
 }
